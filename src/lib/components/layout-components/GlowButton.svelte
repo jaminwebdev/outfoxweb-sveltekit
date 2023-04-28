435167ed272a51b483498cfae153aa0f8fa18a2a
Jamin <jaminwebdev@gmail.com>
2023-04-28 11:08:58 -0700

    Glow button v1

---
+++ src/lib/components/layout-components/GlowButton.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+	export let color: 'primary' | 'secondary' | 'tertiary' = 'primary';
+	export let link: string;
+
+	const colorVariants = {
+		primary: 'bg-primary shadow-[0_0_0.5em_0] shadow-primary after:shadow-[0_0_2.25rem] after:shadow-primary',
+		secondary: 'bg-secondary shadow-[0_0_.5em_0] shadow-secondary after:shadow-[0_0_2.25rem] after:shadow-secondary',
+		tertiary: 'bg-tertiary shadow-[0_0_.5em_0] shadow-tertiary after:shadow-[0_0_2.25rem] after:shadow-tertiary'
+	};
+</script>
+
+<a
+	href={link}
+	class={`glowBtn 
+	${colorVariants[color]} 
+	py-[16px] 
+	px-[28px] 
+	rounded-lg 
+	inline-block 
+	text-body-text-dark 
+	relative 
+	font-medium 
+	cursor-pointer`}
+>
+	<slot />
+</a>
+
+<style>
+	.glowBtn::before {
+		pointer-events: none;
+		content: '';
+		position: absolute;
+		background: inherit;
+		top: 80%;
+		left: 0;
+		width: 100%;
+		height: 100%;
+
+		transform: perspective(40px) rotateX(40deg) scale(1, 0.35);
+		filter: blur(20px);
+		opacity: 0.3;
+		transition: opacity 100ms linear;
+	}
+
+	.glowBtn::after {
+		content: '';
+		position: absolute;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		border-radius: inherit;
+		z-index: -1;
+		opacity: 0;
+		transition: opacity 100ms linear;
+	}
+
+	.glowBtn:hover::before,
+	.glowBtn:focus::before {
+		opacity: 1;
+	}
+	.glowBtn:hover::after,
+	.glowBtn:focus::after {
+		opacity: 0.5;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,11 @@
+<script>
+	import GlowButton from '../lib/components/layout-components/GlowButton.svelte';
+</script>
+
 <div class="h-screen">
 	<h1>Welcome to SvelteKit</h1>
 	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	<div class="grid place-items-center p-7">
+		<GlowButton color="primary" link="/">Click me!</GlowButton>
+	</div>
 </div>

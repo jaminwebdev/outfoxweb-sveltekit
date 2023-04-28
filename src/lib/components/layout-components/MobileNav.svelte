81b9f45da7636453a532d4e84e6b6d17443718f5
Jamin <jaminwebdev@gmail.com>
2023-04-28 11:08:48 -0700

    MobileNav browser api guard

--- src/lib/components/layout-components/MobileNav.svelte
+++ src/lib/components/layout-components/MobileNav.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import Hamburger from './Hamburger.svelte';
 	import Sidebar from './Sidebar.svelte';
+	import { browser } from '$app/environment';
 
 	let open = false;
 
 	const toggleOpen = () => (open = !open);
 
-	$: {
+	$: if (browser) {
 		const body = document.querySelector('body');
 		open ? (body!.style.overflowY = 'hidden') : (body!.style.overflowY = 'auto');
 	}

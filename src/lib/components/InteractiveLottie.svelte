d878fe74ebf496c323df38a5048ccdf07c2cb473
Jamin <jaminwebdev@gmail.com>
2023-05-02 15:23:57 -0700

    InteractiveLottie component created

---
+++ src/lib/components/InteractiveLottie.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { Lottie } from 'lottie-svelte';
+	import type { AnimationEvent, AnimationItem } from 'lottie-svelte/iface';
+
+	export let timeLoop = 6000;
+	export let repeat = false;
+	export let path = '/lottie/Successful.json';
+	export let hover = false;
+
+	let animation: AnimationItem;
+
+	const handler = (event: AnimationEvent) => {
+		animation = event.detail;
+		animation.pause();
+		if (repeat) {
+			setInterval(() => animation.goToAndPlay(0), timeLoop);
+		}
+	};
+
+	$: if (hover) {
+		animation.goToAndPlay(0);
+	}
+</script>
+
+<!-- Lottie file is located at static/heart.json -->
+<Lottie bind:path loop={false} on:animation={handler} />
--- src/routes/showcase/+page.svelte
+++ src/routes/showcase/+page.svelte
@@ -2,18 +2,45 @@
 	import MultiStepProjectForm from '$lib/components/forms/multi-step-project/MultiStepProjectForm.svelte';
 	import GlowButton from '$lib/components/GlowButton.svelte';
 	import Button from '$lib/components/Button.svelte';
+	import InteractiveLottie from '$lib/components/InteractiveLottie.svelte';
+
+	let hovering = false;
+	$: play = hovering;
 </script>
+
+<div class="grid grid-cols-3 my-8 gap-5 max-w-4xl mx-auto">
+	<!-- Example of outer card hover controlling lottie animation -->
+	<div
+		class="bg-bg-color-dark rounded-xl"
+		on:mouseenter={() => (hovering = true)}
+		on:mouseleave={() => (hovering = false)}
+	>
+		<InteractiveLottie path="/lottie/Successful.json" bind:hover={play} />
+	</div>
+	<!-- Example of repeating lottie animation -->
+	<div class="bg-bg-color-dark rounded-xl">
+		<InteractiveLottie path="/lottie/Bubbles.json" timeLoop={5000} repeat />
+	</div>
+</div>
+
+<div class="max-w-4xl m-auto">
+	<h2>Multi-step form</h2>
+	<MultiStepProjectForm />
+</div>
 
 <div class="grid place-items-center p-7">
 	<h2>Glow Buttons</h2>
 	<div class="grid gap-3 grid-cols-3">
 		<GlowButton classes="mt-6" link="/" type="link">Glow Button Link</GlowButton>
-		<GlowButton classes="mt-6 text-body-text-light" on:glowBtnClicked={console.log} color="tertiary">Glow Button Action</GlowButton>
-		<GlowButton classes="mt-6 text-body-text-light" on:glowBtnClicked={console.log} color="secondary">Glow Button Action</GlowButton>
+		<GlowButton classes="mt-6 text-body-text-light" on:glowBtnClicked={console.log} color="tertiary"
+			>Glow Button Action</GlowButton
+		>
+		<GlowButton
+			classes="mt-6 text-body-text-light"
+			on:glowBtnClicked={console.log}
+			color="secondary">Glow Button Action</GlowButton
+		>
 	</div>
-</div>
-<div class="max-w-4xl m-auto">
-	<MultiStepProjectForm />
 </div>
 <div class="grid gap-6 place-items-center p-7">
 	<h2>Regular Buttons - regular</h2>

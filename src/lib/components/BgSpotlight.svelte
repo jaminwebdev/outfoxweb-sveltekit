49d612b15c106681f1fade9f7455f251418c4cc1
Jamin <jaminwebdev@gmail.com>
2023-05-04 13:19:40 -0700

    bg spotlight

---
+++ src/lib/components/BgSpotlight.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	export let classes = '';
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	class="{classes} pointer-events-none"
+	viewBox="0 0 997 848"
+	fill="none">
+	<g filter="url(#filter0_f_137_4)">
+		<path
+			fill-rule="evenodd"
+			clip-rule="evenodd"
+			d="M478.828 246.005C504.675 245.534 516.975 278.894 540.204 290.24C571.631 305.589 618.042 293.956 637.078 323.296C655.649 351.92 645.962 394.837 626.043 422.541C607.395 448.478 568.219 444.043 539.787 458.609C517.722 469.914 503.602 496.982 478.828 497.962C453.851 498.95 432.83 480.307 414.26 463.575C397.323 448.313 388.14 427.938 378.294 407.374C366.722 383.205 343.021 358.753 351.978 333.498C360.96 308.173 396.936 307.704 419.056 292.447C440.347 277.762 452.967 246.476 478.828 246.005Z"
+			fill="#62E9C3"
+			fill-opacity="0.6" />
+	</g>
+	<defs>
+		<filter
+			id="filter0_f_137_4"
+			x="0"
+			y="-104"
+			width="997"
+			height="952"
+			filterUnits="userSpaceOnUse"
+			color-interpolation-filters="sRGB">
+			<feFlood flood-opacity="0" result="BackgroundImageFix" />
+			<feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+			<feGaussianBlur stdDeviation="175" result="effect1_foregroundBlur_137_4" />
+		</filter>
+	</defs>
+</svg>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,17 @@
 <script>
 	import '../app.css';
-	import Navigation from '../lib/components/layout-components/Navigation.svelte';
-	import Footer from '../lib/components/layout-components/Footer.svelte';
+	import Navigation from '$lib/components/layout-components/Navigation.svelte';
+	import Footer from '$lib/components/layout-components/Footer.svelte';
+	import BgSpotlight from '$lib/components/BgSpotlight.svelte';
 </script>
 
-<div class="max-w-[1200px] mx-auto px-5">
-	<Navigation />
-	<slot />
-	<Footer />
+<div class="relative overflow-x-hidden">
+	<BgSpotlight
+		classes="absolute -top-[30vw] -right-[34%] md:-top-[420px] md:-right-[40%] w-[80%]" />
+
+	<div class="max-w-[1200px] mx-auto px-5">
+		<Navigation />
+		<slot />
+		<Footer />
+	</div>
 </div>

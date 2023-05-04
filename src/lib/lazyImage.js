379419d34718d815919bee603d13ed772be612f5
Jamin <jaminwebdev@gmail.com>
2023-05-04 16:26:53 -0700

    lazy image directive started

---
+++ src/lib/lazyImage.js
@@ -0,0 +1,20 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import viewportAction from './useViewportAction';
+
+export default function lazyimage(element) {
+	const viewportActionMethods = viewportAction(element);
+	element.addEventListener('enterViewport', enterViewport);
+
+	function enterViewport() {
+		element.src = element.dataset.src;
+		element.removeEventListener('enterViewport', enterViewport);
+	}
+
+	return {
+		destroy() {
+			viewportActionMethods.destroy();
+			element.removeEventListener('enterViewport', enterViewport);
+		}
+	};
+}
---
+++ src/lib/useViewportAction.js
@@ -0,0 +1,26 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+let intersectionObserver;
+
+function ensureIntersectionObserver() {
+	if (intersectionObserver) return;
+
+	intersectionObserver = new IntersectionObserver((entries) => {
+		entries.forEach((entry) => {
+			const eventName = entry.isIntersecting ? 'enterViewport' : 'exitViewport';
+			entry.target.dispatchEvent(new CustomEvent(eventName));
+		});
+	});
+}
+
+export default function viewport(element) {
+	ensureIntersectionObserver();
+
+	intersectionObserver.observe(element);
+
+	return {
+		destroy() {
+			intersectionObserver.unobserve(element);
+		}
+	};
+}

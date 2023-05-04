171a83393bea1c0a10182b4faca275240d2d9b9f
Jamin <jaminwebdev@gmail.com>
2023-05-04 13:57:21 -0700

    code cleanup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
 	import '../app.css';
-	import Navigation from '$lib/components/layout-components/Navigation.svelte';
-	import Footer from '$lib/components/layout-components/Footer.svelte';
+	import Navigation from '$lib/components/layout/Navigation.svelte';
+	import Footer from '$lib/components/layout/Footer.svelte';
 	import BgSpotlight from '$lib/components/BgSpotlight.svelte';
 </script>
 
@@ -9,7 +9,7 @@
 	<BgSpotlight
 		classes="absolute -top-[30vw] -right-[34%] md:-top-[420px] md:-right-[40%] w-[80%]" />
 
-	<div class="max-w-[1200px] mx-auto px-5">
+	<div id="layoutContainer" class="max-w-[1200px] mx-auto px-5">
 		<Navigation />
 		<slot />
 		<Footer />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,3 @@
-<script>
-	import MultiStepProjectForm from '$lib/components/forms/multi-step-project/MultiStepProjectForm.svelte';
-	import GlowButton from '$lib/components/GlowButton.svelte';
-	import Button from '$lib/components/Button.svelte';
-</script>
-
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 

75d81679ff2f13788339f69fac85035a7cb05a87
Jamin <jaminwebdev@gmail.com>
2023-05-04 16:26:32 -0700

    image processing example and image bg cloud comp

---
+++ src/lib/components/ImgBackground.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	export let classes = '';
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width="100%"
+	height="100%"
+	viewBox="0 0 761 692"
+	fill="none"
+	class={classes}>
+	<g opacity="0.8" filter="url(#filter0_f_175_5)">
+		<path
+			fill-rule="evenodd"
+			clip-rule="evenodd"
+			d="M445.488 200.003C463.242 199.68 471.69 222.582 487.645 230.371C509.231 240.908 541.109 232.922 554.185 253.064C566.941 272.715 560.287 302.178 546.605 321.197C533.796 339.003 506.888 335.958 487.359 345.958C472.203 353.719 462.504 372.301 445.488 372.974C428.332 373.652 413.893 360.854 401.138 349.367C389.504 338.889 383.197 324.902 376.435 310.785C368.486 294.192 352.206 277.406 358.359 260.068C364.528 242.682 389.239 242.36 404.432 231.886C419.056 221.805 427.725 200.327 445.488 200.003Z"
+			fill="#8E59ED" />
+	</g>
+	<g opacity="0.8" filter="url(#filter1_f_175_5)">
+		<path
+			fill-rule="evenodd"
+			clip-rule="evenodd"
+			d="M316.808 358.067C327.648 368.52 318.981 387.32 323.878 401.559C330.504 420.823 354.409 435.15 350.172 455.07C346.037 474.504 324.379 488.182 304.769 491.383C286.409 494.379 272.099 476.417 254.391 470.703C240.648 466.268 223.688 471.596 213.08 461.794C202.385 451.913 201.402 435.579 200.641 421.042C199.948 407.782 204.554 395.612 208.964 383.09C214.147 368.373 214.451 348.544 228.538 341.837C242.664 335.11 257.675 349.736 273.068 352.566C287.883 355.29 305.962 347.609 316.808 358.067Z"
+			fill="#198CE8" />
+	</g>
+	<defs>
+		<filter
+			id="filter0_f_175_5"
+			x="157"
+			y="0"
+			width="604"
+			height="573"
+			filterUnits="userSpaceOnUse"
+			color-interpolation-filters="sRGB">
+			<feFlood flood-opacity="0" result="BackgroundImageFix" />
+			<feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+			<feGaussianBlur stdDeviation="100" result="effect1_foregroundBlur_175_5" />
+		</filter>
+		<filter
+			id="filter1_f_175_5"
+			x="0.571289"
+			y="140.086"
+			width="550.101"
+			height="551.632"
+			filterUnits="userSpaceOnUse"
+			color-interpolation-filters="sRGB">
+			<feFlood flood-opacity="0" result="BackgroundImageFix" />
+			<feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+			<feGaussianBlur stdDeviation="100" result="effect1_foregroundBlur_175_5" />
+		</filter>
+	</defs>
+</svg>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	// @ts-nocheck
+	import Image from '$lib/images/homepage_hero_img.png?format=webp&quality=75&w=550';
+	import GlowButton from '$lib/components/GlowButton.svelte';
+	import ImgBackground from '$lib/components/ImgBackground.svelte';
+</script>
 
-<a href="/showcase">Showcase</a>
+<div class="grid grid-cols-2 items-center gap-14 py-24">
+	<div class="grid gap-7">
+		<h1>Grow Better <br />& Engage More</h1>
+		<p>
+			Reach your audience, convert more customers, and rank better with the best website for you.
+		</p>
+		<div>
+			<GlowButton type="link" link="/showcase" classes="justify-self-start">Showcase</GlowButton>
+		</div>
+	</div>
+	<div class="relative">
+		<img
+			src={Image}
+			alt="laptop and mobile phone mockup of modern website"
+			class="justify-self-end relative z-[1]" />
+		<ImgBackground classes="absolute -z-0 -top-[90px] -right-[170px] w-[150%] h-[150%]" />
+	</div>
+</div>
+<div class="h-[500px]" />

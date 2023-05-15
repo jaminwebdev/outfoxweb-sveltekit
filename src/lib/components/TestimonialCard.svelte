a46912c51a93d45c6c78fbef21d40663f27560f7
Jamin <jaminwebdev@gmail.com>
2023-05-15 16:00:13 -0700

    testimonial card mobile styles

--- src/lib/components/TestimonialCard.svelte
+++ src/lib/components/TestimonialCard.svelte
@@ -5,12 +5,13 @@
 </script>
 
 <div
-	class="rounded-2xl p-6 grid grid-cols-1 sm:grid-cols-[85px,_1fr] gap-5 items-center bg-secondary-bg-light dark:bg-[#2E3438] {classes}">
-	<div class="rounded-full overflow-hidden w-[80px] h-[80px] -mt-[65px] sm:mt-0">
+	class="rounded-2xl p-6 grid grid-cols-1 sm:grid-cols-[85px,_1fr] gap-0 sm:gap-5 items-center bg-secondary-bg-light dark:bg-[#2E3438] {classes}">
+	<div
+		class="rounded-full overflow-hidden w-[80px] h-[80px] -mt-[65px] sm:mt-0 justify-self-center sm:justify-self-start">
 		<img src={testimonial.imgPath} alt="{testimonial.name}'s headshot" class="object-cover" />
 	</div>
 	<div>
-		<h4>{testimonial.name}</h4>
+		<h4 class="text-center sm:text-left mt-2 sm:mt-0 mb-4 sm:mb-0">{testimonial.name}</h4>
 		<p>{testimonial.body}</p>
 	</div>
 </div>

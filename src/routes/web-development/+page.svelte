342225a331848a4c10b63e985a6946c6c9a48663
Jamin <jaminwebdev@gmail.com>
2023-05-20 14:02:02 -0700

    webdev intro row

--- src/routes/web-development/+page.svelte
+++ src/routes/web-development/+page.svelte
@@ -4,6 +4,7 @@
 	import ImgBackground from '$lib/components/ImgBackground.svelte';
 	import BenefitCard from '$lib/components/BenefitCard.svelte';
 	import Checkmark from '$lib/components/Checkmark.svelte';
+	import CoworkImg from '$lib/images/Cowork_Header.png?format=webp&quality=100&w=550';
 </script>
 
 <header class="grid grid-cols-1 md:grid-cols-2 items-center gap-32 md:gap-14 py-24">
@@ -47,4 +48,30 @@
 			Gain a competitive advantage, increase brand awareness, and reach your goals.
 		</span>
 	</BenefitCard>
+</section>
+<section
+	class="rounded-xl bg-secondary-bg-light dark:bg-secondary-bg-dark px-6 py-10 md:p-[60px] grid grid-cols-1 lg:grid-cols-2 gap-4 items-center overflow-hidden">
+	<!-- Left column -->
+	<div>
+		<p class="bg-orange/20 text-orange py-[5px] px-[14px] rounded-md inline-block text-base mb-2">
+			POOR PERFORMANCE?
+		</p>
+		<h2 class="mb-4">Is your website performing?</h2>
+		<p class="mb-8 lg:mb-4">
+			What’s worse than a slow website? A slow website that doesn’t bring in customers, looks
+			outdated, and ultimately doesn’t fit your business needs.
+		</p>
+		<p class="mb-8 lg:mb-4">
+			It’s no secret: research shows people are quick to abandon slow and poorly designed websites.
+			This is precisely why we’ve spent years crafting our tech stack and honing our website
+			building process.
+		</p>
+	</div>
+	<!-- Right column -->
+	<img
+		src={CoworkImg}
+		width="602"
+		height="513"
+		alt="Coworking business website design"
+		class="max-h-[380px] lg:max-h-[470px] -mb-[80px] lg:ml-[80px] object-contain justify-self-center lg:justify-self-auto" />
 </section>

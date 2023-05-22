bdfd6ada00e791b4275bb6137b3a857d9a5b1ec8
Jamin <jaminwebdev@gmail.com>
2023-05-22 15:02:08 -0700

    footer spotlight

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 	import BgSpotlight from '$lib/components/BgSpotlight.svelte';
 </script>
 
-<div class="relative overflow-x-hidden">
+<div class="relative overflow-hidden">
 	<BgSpotlight
 		classes="absolute -top-[30vw] -right-[34%] md:-top-[420px] md:-right-[40%] w-[80%]" />
 
@@ -14,4 +14,5 @@
 		<slot />
 		<Footer />
 	</div>
+	<BgSpotlight classes="absolute -bottom-[200px] -left-[375px] w-[800px] rotate-180" />
 </div>

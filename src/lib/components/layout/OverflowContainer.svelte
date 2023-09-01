a02d71ba4f32df3ec9416ddb7557c3a582f2110a
Jamin <jaminwebdev@gmail.com>
2023-09-01 13:01:53 -0700

    layout cleanup

--- src/lib/components/layout/OverflowContainer.svelte
+++
@@ -1,5 +0,0 @@
-<div class="overflow-hidden">
-	<div class="max-w-[1200px] mx-auto px-6">
-		<slot />
-	</div>
-</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 	import Navigation from '$lib/components/layout/navigation/Navigation.svelte';
 	import Footer from '$lib/components/layout/Footer.svelte';
 	import BgSpotlight from '$lib/components/BgSpotlight.svelte';
-	import OverflowContainer from '$lib/components/layout/OverflowContainer.svelte';
+	import Container from '$lib/components/layout/Container.svelte';
 	import type { LayoutData } from './$types';
 	export let data: LayoutData;
 </script>
@@ -15,11 +15,11 @@
 			classes="absolute -top-[30vw] -right-[34%] md:-top-[420px] md:-right-[40%] w-[80%]" />
 	</div>
 
-	<OverflowContainer>
+	<Container>
 		<Navigation urlData={data.url} />
 		<slot />
 		<Footer />
-	</OverflowContainer>
+	</Container>
 	<div
 		class="w-screen h-[500px] md:h-[700px] overflow-hidden absolute bottom-0 pointer-events-none">
 		<BgSpotlight classes="absolute -bottom-[200px] -left-[375px] w-[800px] rotate-180" />

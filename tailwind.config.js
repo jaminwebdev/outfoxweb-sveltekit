c6036f1c53d56edbda54da2f6dfde0de0bc489a1
Jamin <jaminwebdev@gmail.com>
2024-10-03 14:12:44 -0500

    rafactor tailwind and primary styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,19 @@
 /** @type {import('tailwindcss').Config} */
-import plugin from 'tailwindcss';
-export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,css}'],
 	darkMode: 'class',
 	theme: {
 		extend: {
 			colors: {
-				primary: '#62E9C3',
-				secondary: '#198CE8',
-				tertiary: '#8E59ED',
-				orange: '#F7754C',
-				'body-color': {
-					light: '#F4F4F5',
-					dark: '#1D1D20'
-				},
-				'body-text': {
-					light: '#F4F4F5',
-					dark: '#1D1D20'
-				},
-				'secondary-bg': {
-					dark: '#1F2326',
-					light: '#E6EDF4'
-				},
-				'tertiary-bg': {
-					dark: '#4C4F51',
-					light: '#cad9e8'
-				}
+				primary: 'rgb(var(--color-primary) / <alpha-value>)',
+				secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
+				tertiary: 'rgb(var(--color-tertiary) / <alpha-value>)',
+				orange: 'rgb(var(--color-orange) / <alpha-value>)',
+				'body-color': 'var(--body-color)',
+				'body-color-secondary': 'var(--body-color-secondary)',
+				'body-color-tertiary': 'var(--body-color-tertiary)',
+				'body-text': 'var(--color-body-text)'
 			}
 		}
-	},
-	plugins: [
-		plugin(function ({ addUtilities }) {
-			addUtilities({
-				'html.dark p': {
-					color: 'text-body-text-light'
-				}
-			});
-		})
-	]
+	}
 };

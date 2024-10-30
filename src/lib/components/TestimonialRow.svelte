462caa98fb2470c1f3d3eab663cfff377a8b3ca8
Jamin <jaminwebdev@gmail.com>
2024-10-30 17:05:12 -0500

    TestimonialRow Svelte5 conversion

--- src/lib/components/TestimonialRow.svelte
+++ src/lib/components/TestimonialRow.svelte
@@ -1,4 +1,4 @@
-<script context="module" lang="ts">
+<script module lang="ts">
 	export type Testimonial = {
 		imgPath: string;
 		name: string;
@@ -8,13 +8,15 @@
 
 <script lang="ts">
 	import TestimonialCard from '$lib/components/cards/TestimonialCard.svelte';
-
-	export let testimonials: [Testimonial, Testimonial];
+	interface Props {
+		testimonials: [Testimonial, Testimonial];
+	}
+	let { testimonials }: Props = $props();
 </script>
 
 <section
 	class="rounded-xl px-6 py-12 md:pt-[60px] md:px-[50px] md:pb-[150px] grid grid-cols-1 lg:grid-cols-2 gap-16 content-center bg-cover bg-center"
-	style="background-image: url('staticImages/Testimonials_Bg.jpg')">
+	style="background-image: url('/images/Testimonials_Bg.jpg')">
 	<div class="grid gap-4">
 		<h3 class="text-4xl">What our happy clients have to say</h3>
 		<p>We pride ourselves in our clients’ ongoing successes.</p>

83af8e5cefa8001cee365b0e9f1f034c4e057589
Jamin <jaminwebdev@gmail.com>
2024-11-01 20:21:17 -0500

    fix TestimonialRow bg img

--- src/lib/components/TestimonialRow.svelte
+++ src/lib/components/TestimonialRow.svelte
@@ -16,7 +16,7 @@
 
 <section
 	class="rounded-xl px-6 py-12 md:pt-[60px] md:px-[50px] md:pb-[150px] grid grid-cols-1 lg:grid-cols-2 gap-16 content-center bg-cover bg-center"
-	style="background-image: url('/images/Testimonials_Bg.jpg')">
+	style="background-image: url('staticImages/Testimonials_Bg.jpg')">
 	<div class="grid gap-4">
 		<h3 class="text-4xl">What our happy clients have to say</h3>
 		<p>We pride ourselves in our clients’ ongoing successes.</p>

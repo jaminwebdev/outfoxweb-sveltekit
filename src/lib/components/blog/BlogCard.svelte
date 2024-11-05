bd14fc643ae4d6a4d961049588b640c7093dc571
Jamin <jaminwebdev@gmail.com>
2024-11-05 16:04:44 -0600

    new blog components

---
+++ src/lib/components/blog/BlogCard.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import type { PostMeta } from '../../utils/markdownPosts';
+
+	interface Props {
+		blog: PostMeta;
+	}
+
+	let { blog }: Props = $props();
+</script>
+
+<div
+	class="rounded-xl overflow-hidden bg-body-color-secondary flex flex-col hover:scale-105 hover:[&_h3]:text-tertiary transition-all duration-300">
+	<a href={blog.path} class="h-full">
+		<div class="h-[265px] overflow-hidden">
+			<img src={blog.meta.coverImage} alt="{blog.meta.title} cover image" class="h-full w-full" />
+		</div>
+		<div class="pt-4 px-4 pb-8 grid">
+			<h3 class="text-2xl">
+				{blog.meta.title}
+			</h3>
+			<div class="flex gap-2 text-primary mb-2">
+				{#each blog.meta.tags as tag}
+					<span>#{tag}</span>
+				{/each}
+			</div>
+			<p>{blog.meta.description}</p>
+		</div>
+	</a>
+</div>
---
+++ src/lib/components/blog/BlogGrid.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { PostMeta } from '$lib/utils/markdownPosts';
+	import BlogCard from './BlogCard.svelte';
+
+	interface Props {
+		blogs: PostMeta[];
+	}
+
+	let { blogs }: Props = $props();
+</script>
+
+<section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-12">
+	{#each blogs as blog (Math.random())}
+		<BlogCard {blog} />
+	{/each}
+</section>

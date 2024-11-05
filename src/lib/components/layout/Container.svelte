001407a276bd1d8395001abcb8adbfdb81a7cb63
Jamin <jaminwebdev@gmail.com>
2024-11-05 16:04:52 -0600

    added classes to Container

--- src/lib/components/layout/Container.svelte
+++ src/lib/components/layout/Container.svelte
@@ -1,3 +1,14 @@
-<div class="max-w-[1200px] mx-auto px-6">
-	<slot />
+<script lang="ts">
+	import type { Snippet } from "svelte";
+
+	interface Props {
+		classes: string;
+    children: Snippet;
+	}
+
+	let { classes, children }: Props = $props();
+</script>
+
+<div class="max-w-[1200px] mx-auto px-6 {classes}">
+	{@render children()}
 </div>
